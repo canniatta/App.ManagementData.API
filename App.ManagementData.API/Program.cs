68eb13b44c4b0e1b109d4c10ea041ff8ad72e8fc
Azhari Agand <azhariagand@outlook.com>
2022-07-05 23:09:47 +0700

    add description for swagger

--- App.ManagementData.API/Program.cs
+++ App.ManagementData.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,7 +12,18 @@ builder.Services.AddCors(options => options.AddDefaultPolicy(
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Management Data API",
+            Version = "v1",
+            Description = "Management Data API"
+        });
+    }
+    );
 
 var app = builder.Build();
 
@@ -18,7 +31,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("v1/swagger.json", "Management Data API");
+    });
 }
 
 app.UseAuthorization();
