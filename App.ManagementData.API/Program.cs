5c6e58953b0c0199b5462741265569d8dc6f8e3a
Azhari Agand <azhariagand@outlook.com>
2022-07-05 23:06:35 +0700

    add method CORS

--- App.ManagementData.API/Program.cs
+++ App.ManagementData.API/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options => options.AddDefaultPolicy(
+                   builder => builder.AllowAnyOrigin()
+                   .AllowAnyHeader()
+                   .AllowAnyMethod()
+               ));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
